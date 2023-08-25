94a8467e7ddecb74e11aa2d75dfda39473eb46f1
Carlos20daniel <1440648@senati.pe>
2023-08-25 12:27:23 -0500

    insertamos metodo mergeSort

--- MargeEXPO/src/main/java/com/mycompany/margeexpo/MargeEXPO.java
+++ MargeEXPO/src/main/java/com/mycompany/margeexpo/MargeEXPO.java
@@ -16,5 +16,12 @@ public class MargeEXPO {
         mergeSort(0, array.length-1);
         printlnArray("Sorted array   = ");
     }
-    
+    private static void mergeSort(int low, int high) {
+        if(low<high){
+            int middle = (low+high)/2;
+            mergeSort(low, middle);
+            mergeSort(middle+1, high);
+            merge(low, middle, high);
+        }// if i==j, la sublista tiene 1 elemento, entonces se considera ordenada
+    }
 }
