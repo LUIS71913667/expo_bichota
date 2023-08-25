2e76d3ac4c181e995308f9c3af9345049e95c385
Carlos20daniel <1440648@senati.pe>
2023-08-25 12:55:53 -0500

    actualizacion

--- MargeEXPO/src/main/java/com/mycompany/margeexpo/MargeEXPO.java
+++ MargeEXPO/src/main/java/com/mycompany/margeexpo/MargeEXPO.java
@@ -12,12 +12,12 @@ public class MargeEXPO {
     static int array[]= {10,6,2,3,11,8,7,10,4,1};
     public static void main(String[] args) {   
         //ingresamos los metodos a usar
-        printlnArray("Original array = ");
-        mergeSort(0, array.length-1);
-        printlnArray("Sorted array   = ");
+        printlnArray("Orginal array = ");
+        mergeSort(0, array.length-1);//longuitud de datos -1
+        printlnArray("Actualizacion array = ");
     }
     private static void mergeSort(int low, int high) {
-        if(low<high){
+        if(low<high){//baja<alta
             int middle = (low+high)/2;
             mergeSort(low, middle);
             mergeSort(middle+1, high);
@@ -29,7 +29,7 @@ public class MargeEXPO {
         //Compara miembros en cada sublista en la extrema izquierda;
         //el elemento más pequeño se "elimina" y almacena en una lista temporal que será una lista ordenada después del ciclo
         for(int i=low, j=middle+1, k=0; (i <= middle) || (j <= high); k++){
-            if(i > middle){
+            if(i > middle){//media
                 tempArray[k] = array[j++];
             }
             else if (j > high){ 
@@ -43,18 +43,20 @@ public class MargeEXPO {
         }
         //aplicar cambios
         int index=low;
+        //repetir datos
         for (Integer value : tempArray) {
             array[index++] = value;
         }
     }
     //Método auxiliar para imprimir la matriz.
     private static void printlnArray(String message) {
+        //almacenar cadenas de caracteres
         StringBuffer arrayStr =  new StringBuffer(message+"[");
         for (int i = 0; i < array.length; i++) {
             arrayStr.append(array[i]);
             if(i < array.length-1){ arrayStr.append(", "); }
         }
-        arrayStr.append("]");
+        arrayStr.append("]");//añadir o insertar contenido al final del elemento
         System.out.println(arrayStr);
     }
 }
